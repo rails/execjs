3514c7b28758f01e6c4fc44f6497190bdd420494
Joshua Peek <josh@joshpeek.com>
2012-05-17 12:54:57 -0500

    Define encode helper in module

--- lib/execjs/module.rb
+++ lib/execjs/module.rb
@@ -34,5 +34,34 @@ module ExecJS
     def windows?
       @windows ||= RbConfig::CONFIG["host_os"] =~ /mswin|mingw/
     end
+
+    if defined? Encoding
+      if (!defined?(RUBY_ENGINE) || (RUBY_ENGINE != "jruby" && RUBY_ENGINE != "rbx"))
+        def encode(string)
+          string.encode('UTF-8')
+        end
+      else
+        # workaround for jruby bug http://jira.codehaus.org/browse/JRUBY-6588
+        # workaround for rbx bug https://github.com/rubinius/rubinius/issues/1729
+        def encode(string)
+          if string.encoding.name == 'ASCII-8BIT'
+            data = string.dup
+            data.force_encoding('utf-8')
+
+            unless data.valid_encoding?
+              raise Encoding::UndefinedConversionError, "Could not encode ASCII-8BIT data #{string.dump} as UTF-8"
+            end
+          else
+            data = string.encode('utf-8')
+          end
+          data
+        end
+      end
+    else
+      # Define no-op on 1.8
+      def encode(string)
+        string
+      end
+    end
   end
 end
