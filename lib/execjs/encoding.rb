20a644ddfc5b66205e663bd15d90204f60d02e35
Joshua Peek <josh@joshpeek.com>
2012-05-20 12:12:33 -0500

    Fix encoding check

--- lib/execjs/encoding.rb
+++ lib/execjs/encoding.rb
@@ -2,7 +2,7 @@ module ExecJS
   # Encodes strings as UTF-8
   module Encoding
     if "".respond_to?(:encode)
-      if defined?(RUBY_ENGINE) && (RUBY_ENGINE == 'jruby' || RUBY_ENGINE == 'rbx')
+      if RUBY_ENGINE == 'jruby' || RUBY_ENGINE == 'rbx'
         # workaround for jruby bug http://jira.codehaus.org/browse/JRUBY-6588
         # workaround for rbx bug https://github.com/rubinius/rubinius/issues/1729
         def encode(string)
