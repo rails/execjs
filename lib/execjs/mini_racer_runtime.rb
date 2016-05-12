79bf796f51fad27a6908d005a7fab70d031257b2
Sam <sam.saffron@gmail.com>
2016-05-12 20:18:39 +1000

    Add new signature to function

--- lib/execjs/mini_racer_runtime.rb
+++ lib/execjs/mini_racer_runtime.rb
@@ -3,7 +3,7 @@ require "execjs/runtime"
 module ExecJS
   class MiniRacerRuntime < Runtime
     class Context < Runtime::Context
-      def initialize(runtime, source = "")
+      def initialize(runtime, source = "", options={})
         source = encode(source)
         @context = ::MiniRacer::Context.new
         translate do
