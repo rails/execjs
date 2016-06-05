b0be19c73ded5f20c2c86c8e546558e09da4e2a5
Joshua Peek <josh@joshpeek.com>
2016-06-05 16:03:21 -0700

    Remove process and other node globals from `this`

--- test/test_execjs.rb
+++ test/test_execjs.rb
@@ -239,16 +239,22 @@ class TestExecJS < Test
 
   def test_node_process_is_undefined
     assert ExecJS.eval("typeof process == 'undefined'")
+    refute ExecJS.eval("'process' in this")
   end
 
   def test_commonjs_vars_are_undefined
     assert ExecJS.eval("typeof module == 'undefined'")
     assert ExecJS.eval("typeof exports == 'undefined'")
     assert ExecJS.eval("typeof require == 'undefined'")
+
+    refute ExecJS.eval("'module' in this")
+    refute ExecJS.eval("'exports' in this")
+    refute ExecJS.eval("'require' in this")
   end
 
   def test_console_is_undefined
     assert ExecJS.eval("typeof console == 'undefined'")
+    refute ExecJS.eval("'console' in this")
   end
 
   def test_timers_are_undefined
@@ -258,6 +264,13 @@ class TestExecJS < Test
     assert ExecJS.eval("typeof clearInterval == 'undefined'")
     assert ExecJS.eval("typeof setImmediate == 'undefined'")
     assert ExecJS.eval("typeof clearImmediate == 'undefined'")
+
+    refute ExecJS.eval("'setTimeout' in this")
+    refute ExecJS.eval("'setInterval' in this")
+    refute ExecJS.eval("'clearTimeout' in this")
+    refute ExecJS.eval("'clearInterval' in this")
+    refute ExecJS.eval("'setImmediate' in this")
+    refute ExecJS.eval("'clearImmediate' in this")
   end
 
   def test_compile_large_scripts
